21f342b23bb79c8c01bb86838a38b5fd77fb8476
sbramwel <s.bramwell@live.com>
2022-07-28 12:04:41 -0400

    added new codewars challenge

---
+++ 8kyu/Is-it-even.js
@@ -0,0 +1,14 @@
+// In this Kata we are passing a number (n) into a function.
+
+// Your code will determine if the number passed is even (or not).
+
+// The function needs to return either a true or false.
+
+// My Solution
+function testEven(n) {
+    if ( n % 2 === 0 ){
+      return true;
+    }
+    else return false;
+ }
+ 
