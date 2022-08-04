8a0e79026a33b8bc6ca9e843ba68dd6eb10e31aa
sbramwel <s.bramwell@live.com>
2022-08-04 15:49:30 -0400

    added new codewars challenge

---
+++ 7kyu/find-the-stray-number.js
@@ -0,0 +1,12 @@
+// You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+// Complete the method which accepts such an array, and returns that single different number.
+// The input array will always be valid! (odd-length >= 3)
+
+// My solution
+let arr = numbers.sort();
+  if (arr[0] !== arr[1]) {
+    return arr[0];
+  } else {
+    return arr[arr.length - 1];
+  }
+
