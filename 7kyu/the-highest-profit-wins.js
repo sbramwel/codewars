545730ac4f810c05fc4e0c495d165f81cfd86a62
sbramwel <s.bramwell@live.com>
2022-08-05 14:12:56 -0400

    added new codewars challenge

---
+++ 7kyu/the-highest-profit-wins.js
@@ -0,0 +1,7 @@
+// Write a function that returns both the minimum and maximum number of the given list/array.
+
+// My solution
+function minMax(arr){
+    return [Math.min(...arr), Math.max(...arr)];
+  }
+  
