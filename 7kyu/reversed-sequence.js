3e184622add1ef260e566b035e49175c1aae50ac
sbramwel <s.bramwell@live.com>
2022-08-08 19:45:36 -0400

    added new codewars challenge

---
+++ 7kyu/reversed-sequence.js
@@ -0,0 +1,9 @@
+// Build a function that returns an array of integers from n to 1 where n>0
+
+// My solution
+const reverseSeq = n => {
+    let arr = [];
+      for (let i=n; i>0; i--) {
+        arr.push(i);
+        } return arr;
+    };
