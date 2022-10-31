18675635aafaa6da07e8cf8ad951b02cef37d467
sbramwel <s.bramwell@live.com>
2022-10-31 10:57:19 -0400

    added new codewars

---
+++ 7kyu/list-filtering.js
@@ -0,0 +1,4 @@
+// In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+// My solution
+return l.filter(x => typeof x === 'number');
